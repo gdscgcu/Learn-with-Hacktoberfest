f66eaa74f2b01d3c4d0b0ed630ac790b37647a19
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2022-10-08 21:32:37 +0530

    Create Counting_Sort.py

---
+++ Python/Counting_Sort.py
@@ -0,0 +1,31 @@
+def countingSort(arr):
+
+    size = len(arr)
+    output = [0] * size
+
+    # count array initialization
+    count = [0] * 10
+
+    # storing the count of each element 
+    for m in range(0, size):
+        count[arr[m]] += 1
+
+    # storing the cumulative count
+    for m in range(1, 10):
+        count[m] += count[m - 1]
+
+    # place the elements in output array after finding the index of each element of original array in count array
+    m = size - 1
+    while m >= 0:
+        output[count[arr[m]] - 1] = arr[m]
+        count[arr[m]] -= 1
+        m -= 1
+
+    for m in range(0, size):
+        arr[m] = output[m]
+
+data = [2,5,7,4,7,4,7,4,7]
+countingSort(data)
+
+print("Sorted Array: ")
+print(data)
