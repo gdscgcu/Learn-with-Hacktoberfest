39b541ade74eb36704e9a91d74a72546bd54b23c
Sadaf Saleem <sadafsaleem5815@gmail.com>
2022-10-12 19:14:54 +0500

    Update digital_clock.js

--- digital_clock.js
+++ digital_clock.js
@@ -2,6 +2,21 @@
 <html>
 <head>
   <title> Digital Clock </title>
+  <style>
+    body{
+      background-color: black;
+    }
+    .Clock{
+      position: absolute;
+      color: green'
+      top: 50%;
+      left: 50%;
+      transform: translateX (-50%);
+      font-size: 60px;
+      font-family: Orbitron;
+    }
+  </style>
+
 </head>
 <body>
 <div id = 'DigitalClock'> </div>
@@ -15,15 +30,33 @@ function showTime() {
   
   if (h == 0){
     h = 12; //12 hours format
-    
+  }
+  
   if (h > 12){
     h = h - 12; //12 hours format
     session = 'PM';
-    
-  if (h < 10){
-    h = 12; //12 hours format
+  } 
+
+  /*
+  if (h < 10){ // 0 - 9
+    h = '0' + h ;
+  }
   
-  document.getElementById('DigitalClock').innerHTML = h + ':' + m + ':' + s;
+  if (m < 10){ // 0 - 9
+    m = '0' + m ;
+  }
+   
+  if (s < 10){ // 0 - 9
+    s = '0' + s ;
+  }
+  */
+  
+  h = (h < 10) ? '0' + h : h;
+  m = (m < 10) ? '0' + m : m;
+  s = (s < 10) ? '0' + s : s;
+  
+  document.getElementById('DigitalClock').innerHTML = h + ':' + m + ':' + s + ' ' + session;
+  setTimeout(showTime, 1000);
 }
 </script>
 </body>
